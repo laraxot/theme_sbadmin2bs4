2be9527974ad672c95c66b7746ed0c9faecbc341
marco76tv <marco.sottana@gmail.com>
2019-05-21 14:06:34 +0200

    start

---
+++ admin/dashboard.blade.php
@@ -0,0 +1,10 @@
+@extends('adm_theme::layouts.app')
+@section('page_heading','DashBoard')
+
+@section('content')
+	<div class="row">
+    @foreach(\Auth::User()->areas as $area)
+        {!! $area->dashboard_widget() !!}
+    @endforeach
+	</div>
+@endsection
