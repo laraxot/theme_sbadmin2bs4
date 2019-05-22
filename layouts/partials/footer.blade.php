94a3d70f9630f66d8f3d5adc3df73ff2e9e02bdb
marco76tv <marco.sottana@gmail.com>
2019-05-22 12:33:00 +0200

    footer con versione di laravel e versione php

--- layouts/partials/footer.blade.php
+++ layouts/partials/footer.blade.php
@@ -1,9 +1,12 @@
 <!-- Footer -->
-			<footer class="sticky-footer bg-white">
-				<div class="container my-auto">
-					<div class="copyright text-center my-auto">
-						<span>Copyright &copy; Your Website 2019</span>
-					</div>
-				</div>
-			</footer>
-			<!-- End of Footer -->
+<footer class="sticky-footer bg-white">
+	<div class="container my-auto">
+		<div class="copyright text-center my-auto">
+			<span>
+			Made with Laravel <b>{{ App()::VERSION }}</b>
+			php <b>{{ phpversion() }}</b>
+			</span>
+		</div>
+	</div>
+</footer>
+<!-- End of Footer -->
