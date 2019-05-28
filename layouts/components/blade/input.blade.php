fc93c1a21b50f21cc357c8137c348c26ff667308
marco76tv <marco.sottana@gmail.com>
2019-05-28 09:39:57 +0200

    .

--- layouts/components/blade/input.blade.php
+++ layouts/components/blade/input.blade.php
@@ -1,5 +1,5 @@
 <div class="form-group{{ $errors->has($name) ? ' has-error' : '' }}">
 	{{ $label }}
 	{{ $input }}
-	<small class="form-text text-muted">{{ trans($lang.'.'.$name.'_help') }} </small>
+	<small class="form-text text-muted">{{ trans($view.'.field.'.$name.'_help') }} </small>
 </div>
