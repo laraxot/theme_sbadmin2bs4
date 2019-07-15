dcd62d17ee6a7fe340c6fcade005ebbebebd2141
marco76tv <marco.sottana@gmail.com>
2019-07-15 18:03:32 +0200

    .

--- layouts/partials/tabs.blade.php
+++ layouts/partials/tabs.blade.php
@@ -1,6 +1,7 @@
 <div class="menu-rest">
+	@foreach($tabs as $tabs_level)
 	<ul class="nav nav-pills mb-3 nav-justified" id="pills-tab" role="tablist">
-		@foreach($tabs as $k=>$v)
+		@foreach($tabs_level as $k=>$v)
 		<li class="nav-item">
 			<a class="nav-link {{ $v->active?'active':'' }}" href="{{ $v->url }}">
 				{{ $v->title }}
@@ -8,4 +9,5 @@
 		</li>
 		@endforeach
 	</ul>
+	@endforeach
 </div>	 
