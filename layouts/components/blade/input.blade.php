ca1b652e51a20c6683cb1641eaa77f0ba8989c90
marco76tv <marco.sottana@gmail.com>
2019-07-29 20:59:37 +0200

    .

--- layouts/components/blade/input.blade.php
+++ layouts/components/blade/input.blade.php
@@ -4,16 +4,18 @@
 <div class="form-group{{ $errors->has($name_dot) ? ' has-error is-invalid' : '' }}">
 	{{ $label }}
 	{{ $input }}
-	{{--
+	{{-- --}}
 	@if ( $errors->has($name_dot) )
 	<div class="alert alert-danger">
         {{ $errors->first($name_dot) }}
 	</div>
 	@endif
-	--}}
+	{{-- --}}
+	{{--
 	@error($name_dot)
     	<div class="alert alert-danger">{{ $message }}</div>
 	@enderror
+	--}}
 	{{--  
 	<small class="form-text text-muted">{{ trans($view.'.field.'.$name.'_help') }} </small>
 	<small id="passwordHelpBlock" class="form-text text-muted">
