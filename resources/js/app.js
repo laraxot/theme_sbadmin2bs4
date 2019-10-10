ad238986e71f2f1349679796de1ba8c5b93c5a7a
marco76tv <marco.sottana@gmail.com>
2019-10-10 22:23:49 +0200

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,87 +1,31 @@
-/**
- * First we will load all of this project's JavaScript dependencies which
- * includes Vue and other libraries. It is a great starting point when
- * building robust, powerful web applications using Vue and Laravel.
- */
-
 require('./bootstrap');
 window.Vue = require('vue');
-//window.Vuetify = require('vuetify');
-//window.VApp = require('vuetify/es5/components/VApp');
-/*
+Vue.config.devtools = true;
 
-Vue.use(Vuetify)
+import Example from './components/ExampleComponent'
+import CookieLaw from './components/CookieLawComponent'
 
-export default new Vuetify({
-  icons: {
-    iconfont: 'mdi', // default - only for display purposes
-  },
-});
-*/
-
-/*
-import Vuetify from 'vuetify';
-import VApp from 'vuetify/es5/components/VApp';
-Vue.use(Vuetify, { components: { VApp } });
-*/
-//Vue.use(Vuetify)
-/*
-import Vue from 'vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.min.css';
-
-const opts = {};
+window.Vuetify = require('vuetify');
 Vue.use(Vuetify);
 
-new Vue({
-  vuetify: new Vuetify(opts)
-}).$mount('#app');
-*/
-/**
- * The following block of code may be used to automatically register your
- * Vue components. It will recursively scan this directory for the Vue
- * components and automatically register them with their "basename".
- *
- * Eg. ./components/ExampleComponent.vue -> <example-component></example-component>
- */
+const opts = {}
 
-// const files = require.context('./', true, /\.vue$/i);
-// files.keys().map(key => Vue.component(key.split('/').pop().split('.')[0], files(key).default));
+export default new Vuetify(opts);
 
-//Vue.component('example-component', require('./components/ExampleComponent.vue').default);
-//Vue.component('date-component', require('./components/DateComponent.vue').default);
-//Vue.component('rating-component', require('./components/RatingComponent.vue').default);
-
-
-/**
- * Next, we will create a fresh Vue application instance and attach it to
- * the page. Then, you may begin adding components to this application
- * or customize the JavaScript scaffolding to fit your unique needs.
- */
-//*
 const app = new Vue({
     el: '#app',
+    vuetify: new Vuetify(),
+    components: { Example,CookieLaw }
 });
-//*/
-/*
-new Vue({
-  Vuetify,
-}).$mount('#app');
-//*/
-// ES6 Modules or TypeScript
-//import Swal from 'sweetalert2'
 
-//import '@github/remote-input-element';
-
-// CommonJS
 var pub_res='.';
 
-require('./lighbox.js'); //magnificPopup is not a function
-require('./modal_ajax.js');
-require('./btnDeleteX2.js');
-require('./flatpickr.js');
+require(pub_res+'/lighbox.js'); //magnificPopup is not a function
+require(pub_res+'/modal_ajax.js');
+require(pub_res+'/btnDeleteX2.js');
+require(pub_res+'/flatpickr.js');
 //require('./sb-admin-2.js');
-require('./typeahead_js.js');
-require('./html5imageupload.js');
+require(pub_res+'/typeahead_js.js');
+require(pub_res+'/html5imageupload.js');
 
-require('./xot.js');
+require(pub_res+'/xot.js');
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,81 +1,36 @@
-let mix = require('laravel-mix');
+const mix = require('laravel-mix');
 require('dotenv').config(
 {
   path:__dirname+'/../../../../.env'
 }
 );
 
-$prefix='../../../../';
-/** 
-* nome tema == nome directory tirarla fuori da script 
-* i font sono "pesanti" percio' valutare se cartella comune
-* /webfa-solid-900.svg son 823 KB
-**/
-$suffix='/themes/sb-admin-2-bs4';
-
-var $resource_root=$prefix;
-var $public_path=$prefix+process.env.MIX_PUBLIC_FOLDER;
-
-console.log('MIX_PUBLIC_FOLDER :');
-console.log(process.env.MIX_PUBLIC_FOLDER);
-
-
-/*
-mix.webpackConfig(webpack => {
-    return {
-        plugins: [
-            new webpack.EnvironmentPlugin (
-                ['MIX_PUBLIC_FOLDER']
-            )
-        ]
-    };
-});
-*/
-
-//console.log(process.env);
-
 require('laravel-mix-polyfill');
-
-mix.options({
- 	purifyCss: false,
-});
 
 mix.autoload({
     jquery: ['$', 'window.jQuery', 'jQuery'], // more than one
-    tether: ['window.Tether', 'Tether'],
-	'tether-shepherd': ['Shepherd'],
-	'popper.js/dist/popper.js': ['Popper'],
-  	sweetalert2:['Swal'],
-  	'magnific-popup':['magnificPopup'],
-  	'multiselect-two-sides':['multiselect'],
     moment: 'moment' // only one
 });
 
-var src = 'resources'; 
-var dest = 'dist'; 
- 
-mix.js(src+'/js/auth.js', dest+'/js')
-	.sass(src+'/sass/auth.scss', dest+'/css',{ outputStyle: 'expanded' });
-  
-mix.js(src+'/js/app.js', dest+'/js')
-   .sass(src+'/sass/app.scss', dest+'/css',{ outputStyle: 'expanded' });
-
-mix.extract([
-   'jquery', 'raphael',
-    //    'datatables.net', 'datatables.net-bs', 'datatables.net-responsive', 'datatables.net-responsive-bs',
-   'vue', 'axios'
-]);
-mix.version();
+mix.js('resources/js/app.js', 'dist/js')
+    .sass('resources/sass/app.scss', 'dist/css');
 
 
-mix.setResourceRoot($resource_root)
-  .setPublicPath($public_path)
-  //.setPublicPath(url('/'))
-  //.webpackConfig(Object.assign(webpack))
-  //.sourceMaps()
-  .polyfill({
+mix.extract(['vue','jquery']);
+
+mix.polyfill({
       enabled: true,
       useBuiltIns: "usage",
       targets: {"firefox": "50", "ie": 11}
-   })
- ;
+});
+
+
+var $prefix='../../../../';
+var $suffix='/themes/sb-admin-2-bs4'
+var $resource_root=$prefix+$suffix;
+var $public_path=$prefix+process.env.MIX_PUBLIC_FOLDER+$suffix;
+
+console.log('public_path :'+$public_path);
+console.log('dirname :'+__dirname);
+$res=mix.copyDirectory(__dirname+'/dist', $public_path+'/dist');
+//console.log($res);
