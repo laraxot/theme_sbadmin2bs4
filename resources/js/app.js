33a9945deecb0753aa4514d3557558e0d0645216
marco76tv <marco.sottana@gmail.com>
2019-10-15 12:21:20 +0200

    .

--- resources/js/app.js
+++ resources/js/app.js
@@ -22,6 +22,12 @@ var pub_res='.';
 
 require(pub_res+'/lighbox.js'); //magnificPopup is not a function
 require(pub_res+'/modal_ajax.js');
+//var full=path.resolve(__dirname+'../../../../../../Modules');
+//console.log('full:');
+//console.log(full);
+//alert(full);
+
+
 require(pub_res+'/btnDeleteX2.js');
 require(pub_res+'/flatpickr.js');
 //require('./sb-admin-2.js');
