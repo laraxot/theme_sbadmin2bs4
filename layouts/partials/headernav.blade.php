356d6d9f727e126b93a4e38ef48e42fd0ae6f7c9
marco76tv <marco.sottana@gmail.com>
2020-01-21 12:10:15 +0100

    .

--- layouts/partials/headernav.blade.php
+++ layouts/partials/headernav.blade.php
@@ -1,5 +1,6 @@
 @php
 	$user_panel=Panel::get(Auth::user());
+	$profile_panel=Panel::get(Auth::user()->profile);
 @endphp
 <!-- Topbar -->
 <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
@@ -49,10 +50,11 @@
 			</a>
 			<!-- Dropdown - User Information -->
 			<div class="dropdown-menu dropdown-menu-right shadow animated--grow-in" aria-labelledby="userDropdown">
-				<a class="dropdown-item" href="#">
+				<a class="dropdown-item" href="{{ $profile_panel->showUrl() }}">
 					<i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
 					Profile
 				</a>
+				{{--  
 				<a class="dropdown-item" href="#">
 					<i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
 					Settings
@@ -61,6 +63,7 @@
 					<i class="fas fa-list fa-sm fa-fw mr-2 text-gray-400"></i>
 					Activity Log
 				</a>
+				--}}
 				<div class="dropdown-divider"></div>
 				{{--  
 				<a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
