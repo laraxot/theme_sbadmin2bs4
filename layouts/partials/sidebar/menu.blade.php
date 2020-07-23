112257d4b47e89b4b106e605a575973247fde028
Malebestia <malebestia@yahoo.it>
2020-07-23 11:19:42 +0200

    .

--- layouts/partials/sidebar/menu.blade.php
+++ layouts/partials/sidebar/menu.blade.php
@@ -4,8 +4,8 @@
 @endphp
 @if(isset($menus[0]))
 	@foreach($menus[0] as $el)
-		<li class="nav-item {{ $el->active?'active':'' }}">
-			<a class="nav-link collapsed vis{{$el->visibility}}" href="#collapse{{$el->id}}" data-toggle="collapse" data-target="#collapse{{$el->id}}" aria-expanded="true" aria-controls="collapse{{$el->id}}">
+		<li class="nav-item {{-- $el->active?'active':'' --}}">
+			<a class="nav-link collapsed vis{{-- $el->visibility --}}" href="#collapse{{$el->id}}" data-toggle="collapse" data-target="#collapse{{$el->id}}" aria-expanded="true" aria-controls="collapse{{$el->id}}">
 				{{--  <i class="fas fa-fw fa-cog"></i> --}}
 				<span>{{$el->nome}}</span>
 			</a>
