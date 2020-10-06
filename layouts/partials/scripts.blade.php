6de1edbfff1a6c6b67eaaada20d9102f6d002303
marco76tv <marco.sottana@gmail.com>
2020-10-06 11:57:35 +0200

    .

--- layouts/partials/scripts.blade.php
+++ layouts/partials/scripts.blade.php
@@ -1,6 +1,17 @@
+@php
+	Theme::add('adm_theme::dist/js/manifest.js',1);
+	Theme::add('adm_theme::dist/js/vendor.js',2);
+    Theme::add('adm_theme::dist/js/app.js',3);
+    if(!isset($lang)){
+        $lang=App::getLocale();
+    }
+    $base_url=url('');
+@endphp
+
+
 <script>
-    var base_url='{{ asset('') }}';
-    var lang='{{ \App::getLocale() }}';
+    var base_url='{{ $base_url }}';
+    var lang='{{ $lang }}';
     {{--  var google_maps_api='{{ config('xra.google.maps.api') }}'; --}}
 @if(Request::has('address'))
     var address ="{{ \Request::input('address') }}";
@@ -10,12 +21,9 @@
     var lng ="{{ \Request::input('lng') }}";
 @endif
 </script>
+
 @stack('scripts_before')
-@php
-	Theme::add('adm_theme::dist/js/manifest.js',1);
-	Theme::add('adm_theme::dist/js/vendor.js',2);
-	Theme::add('adm_theme::dist/js/app.js',3);
-@endphp
+
 
 {!! Theme::showScripts(false) !!}
 @livewireScripts
