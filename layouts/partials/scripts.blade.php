321ac72c57dabe75780dfaa13f63582d46e9e60c
marco76tv <marco.sottana@gmail.com>
2020-10-06 11:30:39 +0200

    .

--- layouts/partials/scripts.blade.php
+++ layouts/partials/scripts.blade.php
@@ -1,41 +1,30 @@
 <script>
-    var base_url='{{ asset('') }}';
-    var lang='{{ \App::getLocale() }}';
-    {{--  var google_maps_api='{{ config('xra.google.maps.api') }}'; --}}
-@if(\Request::has('address'))
-    var address ="{{ \Request::input('address') }}";
-@endif
-@if(\Request::has('lat') && \Request::has('lng'))
-    var lat ="{{ \Request::input('lat') }}";
-    var lng ="{{ \Request::input('lng') }}";
-@endif
+    var base_url = '{{ url('
+    ') }}';
+    var lang = '{{ \App::getLocale() }}'; <
+    ? php /* blade_comment_start */ ? >
+        var google_maps_api = '{{ config('
+    xra.google.maps.api ') }}' < ? php /* blade_comment_end */ ? >
+        <
+        beautify start = "@if "
+    exp = "^^^\Request::has('address')^^^" >
+        var address = "{{ \Request::input('address') }}"; <
+    /beautify end="@endif"> <
+    beautify start = "@if "
+    exp = "^^^\Request::has('lat') && \Request::has('lng')^^^" >
+        var lat = "{{ \Request::input('lat') }}";
+    var lng = "{{ \Request::input('lng') }}"; <
+    /beautify end="@endif">
+
 </script>
 @stack('scripts_before')
 @php
-	// Bootstrap core JavaScript
-	//Theme::add('/theme/pub/vendor/jquery/jquery.min.js',1);
-/*
-	Theme::add("/theme/bc/jquery/dist/jquery.min.js",1);
-
-	Theme::add('/theme/vendor/bootstrap/js/bootstrap.bundle.min.js',2);
-	//Core plugin JavaScript
-	Theme::add('/theme/vendor/jquery-easing/jquery.easing.min.js',3);
-	//Custom scripts for all pages
-	Theme::add('/theme/js/sb-admin-2.min.js',4);
-	Theme::add('/theme/js/xot.js');
-*/
-	Theme::add('adm_theme::dist/js/manifest.js',1);
-	Theme::add('adm_theme::dist/js/vendor.js',2);
-	Theme::add('adm_theme::dist/js/app.js',3);
+// Bootstrap core JavaScript
+Theme::add('adm_theme::dist/js/manifest.js',1);
+Theme::add('adm_theme::dist/js/vendor.js',2);
+Theme::add('adm_theme::dist/js/app.js',3);
 @endphp
 
 {!! Theme::showScripts(false) !!}
 @livewireScripts
 @stack('scripts')
-{{--  test funzionamento
-<script>
-	$( document ).ready(function () {
-		Swal.fire('Hello world!');
-	});
-</script>
---}}
