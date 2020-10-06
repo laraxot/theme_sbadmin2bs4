0b43af0c336877d1b5345d0af79b843363111ce1
marco76tv <marco.sottana@gmail.com>
2020-10-06 11:54:41 +0200

    .

--- layouts/partials/scripts.blade.php
+++ layouts/partials/scripts.blade.php
@@ -1,28 +1,20 @@
 <script>
-    var base_url = '{{ url('
-    ') }}';
-    var lang = '{{ \App::getLocale() }}'; <
-    ? php /* blade_comment_start */ ? >
-        var google_maps_api = '{{ config('
-    xra.google.maps.api ') }}' < ? php /* blade_comment_end */ ? >
-        <
-        beautify start = "@if "
-    exp = "^^^\Request::has('address')^^^" >
-        var address = "{{ \Request::input('address') }}"; <
-    /beautify end="@endif"> <
-    beautify start = "@if "
-    exp = "^^^\Request::has('lat') && \Request::has('lng')^^^" >
-        var lat = "{{ \Request::input('lat') }}";
-    var lng = "{{ \Request::input('lng') }}"; <
-    /beautify end="@endif">
-
+    var base_url='{{ asset('') }}';
+    var lang='{{ \App::getLocale() }}';
+    {{--  var google_maps_api='{{ config('xra.google.maps.api') }}'; --}}
+@if(Request::has('address'))
+    var address ="{{ \Request::input('address') }}";
+@endif
+@if(Request::has('lat') && \Request::has('lng'))
+    var lat ="{{ \Request::input('lat') }}";
+    var lng ="{{ \Request::input('lng') }}";
+@endif
 </script>
 @stack('scripts_before')
 @php
-// Bootstrap core JavaScript
-Theme::add('adm_theme::dist/js/manifest.js',1);
-Theme::add('adm_theme::dist/js/vendor.js',2);
-Theme::add('adm_theme::dist/js/app.js',3);
+	Theme::add('adm_theme::dist/js/manifest.js',1);
+	Theme::add('adm_theme::dist/js/vendor.js',2);
+	Theme::add('adm_theme::dist/js/app.js',3);
 @endphp
 
 {!! Theme::showScripts(false) !!}
