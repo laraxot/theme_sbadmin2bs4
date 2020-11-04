c8a258ad8f61de9bb56b6b2a27b98eb4cbfe19c2
marco76tv <marco.sottana@gmail.com>
2020-11-04 09:45:20 +0100

    .

--- Resources/views/layouts/app.blade.php
+++ Resources/views/layouts/app.blade.php
@@ -19,7 +19,9 @@
                         {!! Theme::include('inner_page',[],get_defined_vars() ) !!}
                         @include('adm_theme::layouts.partials.breadcrumb',['_panel'=>$_panel])
                         @include('adm_theme::layouts.partials.tabs',['tabs'=>$_panel->getTabs()])
-                        {!! Theme::include('topbar', [], get_defined_vars()) !!}
+                        {{--
+                            {!! Theme::include('topbar', [], get_defined_vars()) !!}
+                        --}}
                     @endif
                     @yield('section')
                     @yield('content')
