c676374c91525a9ffe54daffaa6e183f4aaa2fcd
marco76tv <marco.sottana@gmail.com>
2021-05-19 11:18:39 +0200

    .

--- Resources/lang/it/txt.php
+++
@@ -1,6 +0,0 @@
-<?php
-
-return [
-    'close' => 'Chiudi',
-    //'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-];
--- Resources/views/errors/404.blade.php
+++
@@ -1,5 +0,0 @@
-@extends('pub_theme::errors.minimal')
-
-@section('title', __('Not Found'))
-@section('code', '404')
-@section('message', $message ?: __('Not Found'))
