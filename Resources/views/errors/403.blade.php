7df8416349d647cab72ffaf1acd40cf29ae88f72
marco76tv <marco.sottana@gmail.com>
2021-05-19 11:19:01 +0200

    .

---
+++ Resources/views/errors/403.blade.php
@@ -0,0 +1,15 @@
+@extends('errors/illustrated-layout')
+
+@section('code', '403')
+@section('title', __('Forbidden'))
+
+@section('image')
+    <div style="background-image: url({{ asset('/svg/403.svg') }});"
+        class="absolute pin bg-cover bg-no-repeat md:bg-left lg:bg-center">
+    </div>
+@endsection
+
+@section('message')
+{{ $msg }}
+{{-- dddx(get_defined_vars()) --}}
+@endsection
