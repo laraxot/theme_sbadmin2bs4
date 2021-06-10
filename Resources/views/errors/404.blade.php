f66fef70053445bb0f94fa2ecbf79c83fbdac288
marco76tv <marco.sottana@gmail.com>
2021-06-10 15:06:48 +0200

    .

--- Resources/views/errors/404.blade.php
+++ Resources/views/errors/404.blade.php
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 @extends('adm_theme::errors.minimal')
-=======
-@extends('pub_theme::errors.minimal')
->>>>>>> c83417ec4b320eede295db3f30b7b4e00751af9c
 
 @section('title', __('Not Found'))
 @section('code', '404')
