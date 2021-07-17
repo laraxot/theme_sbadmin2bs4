7b155ec49986b9b4b76dfc41dc13fdf330448684
Malebestia <malebestia@yahoo.it>
2021-07-17 11:34:47 +0200

    .

--- Resources/views/admin/dashboard.blade.php
+++ Resources/views/admin/dashboard.blade.php
@@ -1,1 +1,16 @@
-eccoci
+@extends('adm_theme::layouts.app')
+@section('page_heading', 'DashBoard')
+
+@section('content')
+    <div class="row">
+        {{-- Guardare NOVA cosa fa --}}
+        {{-- da ripristinare
+        @foreach (\Auth::User()->areas as $area)
+            {!! $area->dashboard_widget() !!}
+        @endforeach
+        @foreach (\Auth::User()->areaAdminAreas as $area)
+            {!! $area->dashboard_widget() !!}
+        @endforeach --}}
+
+    </div>
+@endsection
