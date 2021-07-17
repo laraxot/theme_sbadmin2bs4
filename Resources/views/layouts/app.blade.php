4d33e51c7d609426e4818e73eaa18734ae904adf
Malebestia <malebestia@yahoo.it>
2021-07-17 11:53:36 +0200

    .

--- Resources/views/layouts/app.blade.php
+++ Resources/views/layouts/app.blade.php
@@ -8,7 +8,26 @@
 
 
             <div id="content">
+                @include('adm_theme::layouts.partials.headernav')
+                <!-- Begin Page Content -->
+                <div class="container-fluid" id="app">
+                    <!-- Page Heading -->
+                    @if (isset($_panel))
+                        {!! Theme::include('inner_page',[],get_defined_vars() ) !!}
+                        @include('adm_theme::layouts.partials.breadcrumb',['_panel'=>$_panel])
+                        @include('adm_theme::layouts.partials.tabs',['tabs'=>$_panel->getTabs()])
+                        {{--
+                            {!! Theme::include('topbar', [], get_defined_vars()) !!}
+                        --}}
+                    @endif
+                    @yield('section')
+                    @yield('content')
+                    @if (isset($_panel))
+                        {!! Theme::include('bottombar', [], get_defined_vars()) !!}
+                    @endif
 
+                </div>
+                <!-- /.container-fluid -->
             </div>
             <!-- End of Main Content -->
             @include('adm_theme::layouts.partials.footer')
