dc748078d0994a7f7e1ae2316674691222064bf6
marco76tv <marco.sottana@gmail.com>
2021-07-18 11:36:54 +0200

    .

--- Resources/views/layouts/partials/sidebar/areas.blade.php
+++ Resources/views/layouts/partials/sidebar/areas.blade.php
@@ -4,8 +4,8 @@
         Areas
     </div>
     @php
-    $panel=Panel::get(\Auth::user());
-    $areas=$panel->areas();
+        $panel = Panel::get(\Auth::user());
+        $areas = $panel->areas();
     @endphp
 
     @foreach ($areas as $area)
@@ -16,14 +16,4 @@
             </a>
         </li>
     @endforeach
-    {{--
-    @foreach (\Auth::User()->areas as $area)
-        <li class="nav-item {{ $area->active ? 'active' : '' }}">
-            <a href="{{ $area->url }}" class="nav-link" title="{{ $area->area_define_name }}">
-                <img src="{{ asset($area->icon_src) }}" width="32" />
-                <span>{{ $area->area_define_name }}</span>
-            </a>
-        </li>
-    @endforeach
-    --}}
 @endif
