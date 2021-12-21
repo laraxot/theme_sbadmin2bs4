b6cdf7da2b56bb4cf1e0019a862f40b9f4194549
marco76tv <marco.sottana@gmail.com>
2021-12-21 12:41:30 +0100

    .

--- Resources/views/layouts/partials/sidebar/areas.blade.php
+++ Resources/views/layouts/partials/sidebar/areas.blade.php
@@ -12,7 +12,7 @@
     @foreach ($profile->areas() as $area)
         <li class="nav-item {{ $area->active ? 'active' : '' }}">
             <a href="{{ $area->url }}" class="nav-link" title="{{ $area->area_define_name }}">
-                <img src="{{ asset($area->icon_src) }}" width="32" />
+                <img src="{{ Theme::asset($area->icon_src) }}" width="32" />
                 <span>{{ $area->area_define_name }}</span>
             </a>
         </li>
