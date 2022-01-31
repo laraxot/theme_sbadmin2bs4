02c5726422651237aa063011415e531403f401f0
marco76tv <marco.sottana@gmail.com>
2022-01-31 13:43:31 +0100

    .

---
+++ View/Components/GuestLayout.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Themes\SbAdmin2Bs4\View\Components;
+
+use Illuminate\View\Component;
+
+class GuestLayout extends Component {
+    /**
+     * Get the view / contents that represents the component.
+     *
+     * @return \Illuminate\View\View
+     */
+    public function render() {
+        return view('layouts.guest');
+    }
+}
