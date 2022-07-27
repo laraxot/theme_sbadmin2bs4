a5dec7ad31bc38e62410e2b466cc1b32e564ea35
marco76tv <marco.sottana@gmail.com>
2022-07-27 15:23:55 +0200

    up

--- Resources/views/layouts/partials/footer.blade.php
+++ Resources/views/layouts/partials/footer.blade.php
@@ -13,9 +13,10 @@
 @php
 /*
 xdebug_set_filter(
-    XDEBUG_FILTER_TRACING, XDEBUG_PATH_EXCLUDE, 
+    XDEBUG_FILTER_TRACING,
+    XDEBUG_PATH_EXCLUDE,
     [ __DIR__ . "/vendor/" ]
-);    
+);
 xdebug_print_function_stack();
-*/
-@endphp 
+//*/
+@endphp
