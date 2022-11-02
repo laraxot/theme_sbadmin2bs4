416013328257da910f47f2f6f505374cdb9b5992
marco76tv <marco.sottana@gmail.com>
2022-11-02 13:44:14 +0100

    up

--- Resources/views/errors/500.blade.php
+++ Resources/views/errors/500.blade.php
@@ -2,4 +2,14 @@
 
 @section('title', __('Server Error'))
 @section('code', '500')
-@section('message', $msg ?? ($message ?? __('Server Error')))
+@section('message')
+ {{  $msg ?? $message ?? __('Server Error') }}
+ {{--  
+ <pre>{{ dddx(debug_backtrace()) }}</pre>
+ --}}
+ <ul>
+ @foreach(debug_backtrace() as $v)
+        <li> {{ $v['file'] }} - {{ $v['line'] }}</li>
+ @endforeach
+ </ul>
+@endsection
