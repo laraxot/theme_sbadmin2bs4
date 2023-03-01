70889d70d8793b8f69e00688201865a4f807ccc0
marco76tv <marco.sottana@gmail.com>
2023-03-01 12:15:59 +0100

    up

--- Config/button.php
+++ Config/button.php
@@ -8,6 +8,38 @@
         'class' => 'btn btn-primary',
       ),
     ),
+    'edit' => 
+    array (
+      'v1' => 
+      array (
+        'icon' => '<i class="fas fa-pencil-alt"></i>',
+        'class' => 'btn btn-primary',
+      ),
+    ),
+    'v1' => 
+    array (
+      'create' => 
+      array (
+        'class' => 'btn btn-primary',
+      ),
+      'edit' => 
+      array (
+        'class' => 'btn btn-primary',
+      ),
+      'delete' => 
+      array (
+        'class' => 'btn btn-danger',
+      ),
+      'detach' => 
+      array (
+        'class' => 'btn btn-primary',
+      ),
+      'show' => 
+      array (
+        'class' => 'btn btn-primary',
+        'icon' => '<i class="far fa-eye"></i>',
+      ),
+    ),
   ),
   'link' => 
   array (
